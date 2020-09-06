9beddd22d9e9ff473df81778fa7d5ee3388584fa
miqui <migmaqer@gmail.com>
2020-09-06 01:29:46 -0400

    ..init..
    Signed-off-by: miqui <migmaqer@gmail.com>

--- app_collection1/target/debug/deps/app_collection1-0d0c8d4211dd5e67.d
+++
@@ -1,5 +0,0 @@
-/home/m_maq/development/learn_rust/app_collection1/target/debug/deps/app_collection1-0d0c8d4211dd5e67.rmeta: src/main.rs
-
-/home/m_maq/development/learn_rust/app_collection1/target/debug/deps/app_collection1-0d0c8d4211dd5e67.d: src/main.rs
-
-src/main.rs:
--- app_collection1/target/debug/deps/app_collection1-5fee837e2497ee1f.d
+++
@@ -1,5 +0,0 @@
-/mnt/c/Users/m_maq/development/learn_rust/app_collection1/target/debug/deps/app_collection1-5fee837e2497ee1f: src/main.rs
-
-/mnt/c/Users/m_maq/development/learn_rust/app_collection1/target/debug/deps/app_collection1-5fee837e2497ee1f.d: src/main.rs
-
-src/main.rs:
